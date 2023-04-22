c7a94c0712e36990c7ef0be254bb22bdbdf695ba
Givaildo Andrade <gil_nm@hotmail.com>
2023-04-22 11:42:02 -0300

    feat:create component camera crud

---
+++ frontendHotel/src/app/views/camera-crud/camera-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-crud',
+  templateUrl: './camera-crud.component.html',
+  styleUrls: ['./camera-crud.component.css']
+})
+export class CameraCrudComponent {
+
+}
