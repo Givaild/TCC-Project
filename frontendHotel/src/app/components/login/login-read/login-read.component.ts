7a27d1eda6652b296060a7cc9a51c1fbfaa4a8cc
Givaildo Andrade <gil_nm@hotmail.com>
2023-05-09 23:58:36 -0300

    feat:create login read component

---
+++ frontendHotel/src/app/components/login/login-read/login-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-read',
+  templateUrl: './login-read.component.html',
+  styleUrls: ['./login-read.component.css']
+})
+export class LoginReadComponent {
+
+}
---
+++ frontendHotel/src/app/components/login/login-read/login.model.ts
@@ -0,0 +1,6 @@
+export interface UserModel {
+    id: string
+    email: string
+    password: string
+    isAdmin: boolean
+}
